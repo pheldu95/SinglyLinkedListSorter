bff6f8029efc784976d6086a36257f15b1e48df3
Duncan Phelps <phelps.duncan@gmail.com>
2020-07-16 13:12:43 -0600

    created a linked list.

--- singlyLinkedListSorter.js
+++ singlyLinkedListSorter.js
@@ -4,3 +4,21 @@ class LinkedListNode {
         this.next = null;
     }
 }
+
+// create the first node
+const head = new LinkedListNode(1);
+
+// add a second node
+head.next = new LinkedListNode(2);
+
+// add a third node
+head.next.next = new LinkedListNode(3);
+
+//add fourth node
+head.next.next.next = new LinkedListNode(4);
+
+let current = head;
+while (current !== null) {
+    console.log(current.data);
+    current = current.next;
+}
